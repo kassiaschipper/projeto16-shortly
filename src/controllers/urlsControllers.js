4cffdd839e64643fb145e75aa5075ce8184bf202
Kássia <kassia_guterres@outlook.com>
2022-10-14 13:31:48 -0300

    delete url

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -4,14 +4,14 @@ import { nanoid } from "nanoid";
 async function shortenUrls(req, res) {
   const { url } = req.body;
 
-  //const { session, user } = res.locals //token e userId
+  const { user } = res.locals;
 
   try {
     const shortUrl = nanoid(8);
 
     const insertLink = await connection.query(
-      `INSERT INTO links (url, "createAt", "shortUrl", "visitCount") VALUES ($1, CURRENT_DATE, $2,$3);`,
-      [url, shortUrl, null]
+      `INSERT INTO links (url, "createAt", "shortUrl", "visitCount", "userId") VALUES ($1, CURRENT_DATE, $2,$3, $4);`,
+      [url, shortUrl, null, user]
     );
 
     res.status(201).send({ shortUrl: shortUrl });
@@ -45,24 +45,58 @@ async function listUrlById(req, res) {
 
 async function openShortUrl(req, res) {
   const { shortUrl } = req.params;
-  
+
   try {
-    const findShortUrl = (await connection.query(`SELECT * FROM links WHERE "shortUrl"=$1;`, [shortUrl])).rows[0];
-        
+    const findShortUrl = (
+      await connection.query(`SELECT * FROM links WHERE "shortUrl"=$1;`, [
+        shortUrl,
+      ])
+    ).rows[0];
+
     if (findShortUrl === undefined) {
       return res.sendStatus(404);
     }
-    
-    const countVisit = await connection.query(`UPDATE links SET "visitCount" = $1 WHERE "shortUrl"='${shortUrl}';`, [findShortUrl.visitCount + 1])
+
+    const countVisit = await connection.query(
+      `UPDATE links SET "visitCount" = $1 WHERE "shortUrl"='${shortUrl}';`,
+      [findShortUrl.visitCount + 1]
+    );
 
     const originalUrl = findShortUrl.url;
 
     return res.redirect(originalUrl);
-
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
   }
 }
 
-export { shortenUrls, listUrlById, openShortUrl };
+async function deleteUrl(req, res) {
+  const { id } = req.params;
+  const { user } = res.locals; //userId
+
+  try {
+    const findUrl = (
+      await connection.query(`SELECT * FROM links WHERE id = $1;`, [id])
+    ).rows[0];
+
+    if (findUrl === undefined) {
+      return res.sendStatus(404);
+    }
+
+    if (findUrl.userId !== user) {
+      return res.sendStatus(401);
+    }
+
+    const deleteUrl = await connection.query(
+      `DELETE FROM links WHERE id = $1;`,
+      [id]
+    );
+    return res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+export { shortenUrls, listUrlById, openShortUrl, deleteUrl };
--- src/routes/urlsRoutes.js
+++ src/routes/urlsRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { authToken} from "../middlewares/authMeddlewares.js"
 import { urlValidation } from "../middlewares/urlsMiddlewares.js"
-import { shortenUrls, listUrlById, openShortUrl } from "../controllers/urlsControllers.js"
+import { shortenUrls, listUrlById, openShortUrl, deleteUrl } from "../controllers/urlsControllers.js"
 
 const router = express.Router();
 router.post("/urls/shorten", authToken, urlValidation, shortenUrls);
 router.get("/urls/:id", listUrlById );
-router.get("/urls/open/:shortUrl",openShortUrl)
+router.get("/urls/open/:shortUrl",openShortUrl);
+router.delete("/urls/:id", authToken, deleteUrl)
 
 export default router;
